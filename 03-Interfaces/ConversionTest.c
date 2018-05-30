faee9b06c5ce9b955573801e663455383dd8a936
fdalmaup <dalmaufacundo@gmail.com>
2018-05-30 20:26:24 -0300

    Update ConversionTest.c

--- 03-Interfaces/ConversionTest.c
+++ 03-Interfaces/ConversionTest.c
@@ -1,17 +1,23 @@
 /*
     ConversionTest.c
-    03-Interfaces & Makefiles
+    02-Interfaces & Makefiles
     Grupo 1
     28052018
 */
 
 #include <stdio.h>
+#include <assert.h>   
 #include "Conversion.h"
 
-char rango(double temperaturaA, double temperaturaB){
-	return ((temperaturaA + 1.0 >= temperaturaB) && (temperaturaA - 1.0 <= temperaturaB));
+char rango(double, double);
+void pruebas(void);
+
+
+int main(){
+	pruebas();
 }
-int pruebas(){
+
+void pruebas(void){
 	assert(rango(0.0, Celsius(32.0)));
 	assert(rango(280.0, Celsius(536.0)));
 	assert(rango(120.0, Celsius(248.0)));
@@ -20,6 +26,6 @@ int pruebas(){
 	assert(rango(248.0, Fahrenheit(120.0)));
 }
 
-int main(){
-	pruebas();
+char rango(double temperaturaA, double temperaturaB){
+	return ((temperaturaA + 1.0 >= temperaturaB) && (temperaturaA - 1.0 <= temperaturaB));
 }
