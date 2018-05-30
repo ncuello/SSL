b74b934a7f673989c8ce7f2c7725a44da1b104d8
santiagomcastro <37627597+santiagomcastro@users.noreply.github.com>
2018-05-30 14:41:57 -0300

    Agrego y completo archivo ConversionTest.c

--- 03-Interfaces/ConversionTest.c
+++ 03-Interfaces/ConversionTest.c
@@ -1,6 +1,25 @@
-#include "Conversion.h"
-#include <assert.h>
+/*
+    ConversionTest.c
+    03-Interfaces & Makefiles
+    Grupo 1
+    28052018
+*/
 
-int main(void){
-	
+#include <stdio.h>
+#include "Conversion.h"
+
+char rango(double temperaturaA, double temperaturaB){
+	return ((temperaturaA + 1.0 >= temperaturaB) && (temperaturaA - 1.0 <= temperaturaB));
+}
+int pruebas(){
+	assert(rango(0.0, Celsius(32.0)));
+	assert(rango(280.0, Celsius(536.0)));
+	assert(rango(120.0, Celsius(248.0)));
+	assert(rango(32.0, Fahrenheit(0.0)));
+	assert(rango(536.0, Fahrenheit(280.0)));
+	assert(rango(248.0, Fahrenheit(120.0)));
+}
+
+int main(){
+	pruebas();
 }
